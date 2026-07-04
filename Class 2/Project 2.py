a989ba3b0ac2d26214073d890bae1295fca45dd1
Ayra <aimlcourse25@gmail.com>
2026-07-04 17:18:51 +0100

    These are the activities

---
+++ Class 2/Project 2.py
@@ -0,0 +1,32 @@
+birthdays = {}
+
+name1 = input("Enter name of friend 1: ")
+birthday1 = input(f"Enter {name1}'s birthday (DD-MM-YYYY): ")
+birthdays[name1] = birthday1
+
+name2 = input("Enter name of friend 2: ")
+birthday2 = input(f"Enter {name2}'s birthday (DD-MM-YYYY): ")
+birthdays[name2] = birthday2
+
+name3 = input("Enter name of friend 3: ")
+birthday3 = input(f"Enter {name3}'s birthday (DD-MM-YYYY): ")
+birthdays[name3] = birthday3
+
+name4 = input("Enter name of friend 4: ")
+birthday4 = input(f"Enter {name4}'s birthday (DD-MM-YYYY): ")
+birthdays[name4] = birthday4
+
+name5 = input("Enter name of friend 5: ")
+birthday5 = input(f"Enter {name5}'s birthday (DD-MM-YYYY): ")
+birthdays[name5] = birthday5
+
+print(name1)
+print(birthday1)
+print(name2)
+print(birthday2)
+print(name3)
+print(birthday3)
+print(name4)
+print(birthday4)
+print(name5)
+print(birthday5)
