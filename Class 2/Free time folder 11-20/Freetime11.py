cc7ee4106283c14be4747f34acf464ba33f50ef4
Ayra <aimlcourse25@gmail.com>
2026-08-02 16:00:19 +0100

    code

--- Class 2/Free time folder 11-20/Freetime11.py
+++ Class 2/Free time folder 11-20/Freetime11.py
@@ -1,5 +1,16 @@
 print()
 print("NHS Blue Light Discount Calculator!")
 print("")
-cost=float(input("Please enter the price of your meal:"))
-percentage=int(input("Please enter the percentage of the discount:"))
+cost=float(input("Please enter the price of your meal(in pounds):"))
+percentage=float(input("Please enter the percentage of the discount:"))
+
+if percentage>14:
+    print("That is not possible, please try again.")
+    exit()
+
+total=percentage/100 * cost
+final=cost-total
+print(f"You have to pay £{final} pounds with the discount!")
+print()
+print("Bye!")
+print()
