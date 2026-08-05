64922914d9fa30f69960787ef0ba267a2fb4810b
Ayra <aimlcourse25@gmail.com>
2026-08-05 13:50:38 +0100

    lot of  codiing

---
+++ Class 2/Homework Projects/Project 8.py
@@ -0,0 +1,9 @@
+name=input("Please enter your real name: ")
+club=input("Please enter your school club name:")
+member_number=8
+points_earned=9.5
+event_count=6
+meeting_hours=1.5
+is_active=True
+
+print("Name:",name,"-> type:",type(name))
