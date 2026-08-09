258f87a8bd30acb9402b6936a0a58b2d0a015d16
Ayra <aimlcourse25@gmail.com>
2026-08-09 11:52:58 +0100

    y

---
+++ Class 2/Homework Projects/Project 11.py
@@ -0,0 +1,8 @@
+print("🧮 Welcome to the power calculator ✖️")
+while True:
+    print()
+    num=int(input("Enter a number:"))
+    power=int(input("Enter another number:"))
+    answer=num**power
+    print(f"{num} to the power of {power} gives you {answer}.")
+    print()
