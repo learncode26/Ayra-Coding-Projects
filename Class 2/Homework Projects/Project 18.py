ecb86eb125e87721b446bae7fae1fd70bae99074
Ayra <aimlcourse25@gmail.com>
2026-09-25 20:27:48 +0100

    some code q

--- Class 2/Homework Projects/Project 18.py
+++ Class 2/Homework Projects/Project 18.py
@@ -1,16 +1,24 @@
-print()
-print("-🕹️  🎮-Text Adventure Game-👾  🎲-")
-name=input("Please enter your name, explorer : ").capitalize().strip()
-print(f"Hello {name} in this game you are stranded in a forest with no way of escaping unless you risk your life to make choices, solve clues and solve the puzzle.\nGood luck,explorer {name}!")
-choice1=int(input("You are trekking through the Amazon Rainforest for days in search of a secret temple built billions of years ago. You are heading in the right direction according to your map.\nHowever, as you admire the view, a stealthy monkey grabs your map!!!\nWhat do you do(1/2)?\n1)Chase after the monkey!\n2)Climb up a tree!"))
-if choice1==1:
-    choice2=int(input("You chase after it: you know you are lost without the map! However, your eye caught sight of a banana tree!\nWhat do you do?(1/2)\n1)You keep going after the monkey, it might run out of your sight if you get distracted!\n2)You grab a banana and lure it back!"))
-    if choice2==1:
-        print("As you chase behind it, the monkey clambers up a tree and disappears behind the foliage! You look around,wondering what to do next when you find a tiger prowling right behind you!\n 💀 GAME OVER! 💀 ")
-    elif choice2==2:
-        print("You grab a banana and the monkey races towards you however following it is the rest of the monkey troop!\nWhat do you do?\n1)Throw the banana away!\n2)Stand still!?")
 
+def total_bill(bill_amount, tip_perc):
+    total = bill_amount * (1 + 0.01 * tip_perc)
+    total = round(total, 2)
+    print(f"Please pay £{total}")
+    return total
+ 
+total_bill(150, 20)
 
-
-
-
+def seating_arrangements(numguests):
+    '''This is a recursive function to find the number of seating arrangements for guests.'''
+ 
+    if numguests == 0 or numguests == 1:
+        return 1
+ 
+    else:
+        return numguests * seating_arrangements(numguests - 1)
+ 
+print(seating_arrangements.__doc__)
+ 
+print("Seating arrangements for 1 guest: ", seating_arrangements(1))
+print("Seating arrangements for 2 guests: ", seating_arrangements(2))
+print("Seating arrangements for 3 guests: ", seating_arrangements(3))
+print("Seating arrangements for 5 guests: ", seating_arrangements(5))
